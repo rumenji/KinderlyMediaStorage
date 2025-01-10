1c3bf22e63c3bf4e3da94339a8865daa5009864b
rumenji <rumen_ji@yahoo.com>
2025-01-10 14:52:22 -0500

    Added support for time in 24-hour format

--- helpers.py
+++ helpers.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 def replace_sides(matrix, value):
     """Replaces the values on the top, right, bottom, and left sides of the default message.
     Used to add a color border to the message. Value is an integer from the character codes in Vestaboard:
@@ -27,3 +29,23 @@ def get_last_name(full_name):
     last_name = last_name[:9]  # Limit to 9 characters
     last_name = last_name.ljust(9)  # Add spaces if less than 9 characters
     return last_name
+
+def is_24_hour_format(time_str):
+    """
+    Checks if the given time string is in 24-hour format or 12-hour format.
+
+    Args:
+        time_str (str): The time string to check - can be in 24 or 12-hour format.
+
+    Returns:
+        A datetime object with the time in 24-hour format.
+    """
+
+    try:
+        # Try parsing the time string using 12-hour format
+        time = datetime.strptime(str(time_str), '%I:%M %p').time()
+        return time
+    except ValueError:
+        # If parsing fails, it's likely in 12-hour format
+        time = datetime.strptime(str(time_str), '%H:%M').time()
+        return time
